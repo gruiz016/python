6445caca6755e35e90f12cd7f6d430b715bb326c
Gruiz016 <meet.gio@icloud.com>
2020-01-20 13:15:45 -0500

    finished recursion

--- Programming Foundations_Algorithms/3 Recursion/countdown.py
+++ Programming Foundations_Algorithms/3 Recursion/countdown.py
@@ -2,12 +2,11 @@
 
 
 def countdown(x):
-    if x == 0:
-        print("Done!")
+    if (x == 0):
+        print("Done")
         return
     else:
         print(x, "...")
         countdown(x-1)
-
 
 countdown(5)
