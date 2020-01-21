ea51998d255a81fe325e72de0e91628e8c9832bb
Gruiz016 <meet.gio@icloud.com>
2020-01-21 10:27:44 -0500

    finished other algorithms

--- Programming Foundations_Algorithms/6 Other Algorithms/Filter.py
+++ Programming Foundations_Algorithms/6 Other Algorithms/Filter.py
@@ -6,13 +6,13 @@ items = ["apple", "pear", "orange", "banana", "apple",
          "orange", "apple", "pear", "banana", "orange",
          "apple", "kiwi", "pear", "apple", "orange"]
 
-# create a hashtable to perform a filter
+# TODO: create a hashtable to perform a filter
 filter = dict()
 
-# loop over each item and add to the hashtable
-for item in items:
-    filter[item] = 0
+# TODO: loop over each item and add to the hashtable
+for key in items:
+    filter[key] = 0
 
-# create a set from the resulting keys in the hashtable
+# TODO: create a set from the resulting keys in the hashtable
 result = set(filter.keys())
 print(result)
