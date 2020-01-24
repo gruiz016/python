a1c2c860dbb0ce4197aa8a7969fa7e901bc67b70
Gruiz016 <meet.gio@icloud.com>
2020-01-24 10:15:11 -0500

    finished loops

--- Python Fundamentals/Python Basics/functions.py
+++ Python Fundamentals/Python Basics/functions.py
@@ -5,14 +5,45 @@
 # define a basic function
 
 
+def function1():
+    print("I am a function")
+
 # function that takes arguments
 
+
+def function2(arg1, arg2):
+    print(arg1, " ", arg2)
 
 # function that returns a value
 
 
+def cube(x):
+    return x*x*x
+
 # function with default value for an argument
 
 
-#function with variable number of arguments
+def power(num, x=1):
+    result = 1
+    for i in range(x):
+        result = result * num
+    return result
 
+
+# function with variable number of arguments
+def multiAdd(*args):
+    result = 0
+    for x in args:
+        result = result + x
+    return result
+
+
+function1()
+print(function1)
+function2(10, 20)
+print(function2(10, 20))
+print(cube(3))
+print(power(2))
+print(power(2, 3))
+print(power(x=3, num=2))
+print(multiAdd(4, 5, 10, 4))
--- Python Fundamentals/Python Basics/loops.py
+++ Python Fundamentals/Python Basics/loops.py
@@ -2,23 +2,37 @@
 # Example file for working with loops
 #
 
+
 def main():
-  x = 0
+    x = 0
 
-  # define a while loop
+    # define a while loop
+    while (x < 5):
+        print(x)
+        x += 1
 
+    # define a for loop
+    for x in range(5, 10):
+        print(x)
 
-  # define a for loop
+    # use a for loop over a collection
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    for d in days:
+        print(d)
 
+    # use the break and continue statements
+    for x in range(5, 10):
+        if (x == 7):
+            break
+        if (x % 2 == 0):
+            continue
+        print(x)
 
-  # use a for loop over a collection
-    
- 
-  # use the break and continue statements
-
-
-  #using the enumerate() function to get index 
+    # using the enumerate() function to get index
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    for i, d in enumerate(days):
+        print(i, d)
 
 
 if __name__ == "__main__":
-  main()
+    main()
