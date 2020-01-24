09af701021f29fa1ec776a9e20c4c07cc2e2bb9d
Gruiz016 <meet.gio@icloud.com>
2020-01-24 13:11:34 -0500

    finished workign with dates and times

--- Python Fundamentals/Working with Dates and Times/calendars.py
+++ Python Fundamentals/Working with Dates and Times/calendars.py
@@ -32,3 +32,16 @@ for day in calendar.day_name:
 # a team meeting on the first Friday of every month.
 # To figure out what days that would be for each month,
 # we can use this script:
+
+print("Team meetings will be on: ")
+for m in range(1, 13):
+    cal = calendar.monthcalendar(2020, m)
+    weekone = cal[0]
+    weektwo = cal[1]
+
+    if weekone[calendar.FRIDAY] != 0:
+        meetday = weekone[calendar.FRIDAY]
+    else:
+        meetday = weektwo[calendar.FRIDAY]
+
+    print("%10s %2d" % (calendar.month_name[m], meetday))
